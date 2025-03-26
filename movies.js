ae67fc999ece3237d61a376e536079060718d744
davividahd <daviricardosantos2007@gmail.com>
2025-03-26 15:30:06 -0300

    movies.js

---
+++ movies.js
@@ -0,0 +1,52 @@
+const DATA = [
+    { id: "01", nome: "O Show de Truman ", notaRotten: "94%", imagem: "truman.jpg"
+
+
+     },
+
+    { id: "02", nome: "Doom: A porta do Inferno ", notaRotten: "18%", imagem: "doom.jpg" 
+
+    },
+    { id: "03", nome: "Mortal Kombat - A Aniquilação ", notaRotten: "4%", imagem: "mortalmerda.jpg" 
+
+    },
+    { id: "04", nome: "A Viagem de Chihiro ", notaRotten: "96%", imagem: "a viagem de chiriho.jpg" 
+
+    },
+    { id: "05", nome: "Meu Amigo Totoro ", notaRotten: "94%", imagem: "meu amigo totoro.jpg" 
+
+    },
+    { id: "06", nome: "Ponyo - Uma Amizade que Veio do Mar ", notaRotten: "91%", imagem: "ponyo.jpg" 
+
+    },
+    { id: "07", nome: "Super-Herói: O Filme ", notaRotten: "16%", imagem: "superhero.jpg" 
+
+    },
+    { id: "08", nome: "Max Steel ", notaRotten: "0%", imagem: "maxsteel.jpg" 
+
+    },
+    { id: "09", nome: "Ursinho Pooh: Sangue e Mel ", notaRotten: "3%", imagem: "ursinho pooh.jpg" 
+
+    },
+    { id: "10", nome: "Madame Teia ", notaRotten: "11%", imagem: "madame teia.jpg" 
+
+    },
+    { id: "11", nome: "Meu Cunhado é Vampiro ", notaRotten: "50%", imagem: "meu cunhadovampiro.jpg"
+
+     },
+    { id: "12", nome: "Emília Peres ", notaRotten: "72%", imagem: "emilia peres.jpg"
+
+     },
+    { id: "13", nome: "As Branquelas ", notaRotten: "15%", imagem: "as branquelas.jpg" 
+
+    },
+    { id: "14", nome: "A Substância ", notaRotten: "89%", imagem: "a substancia.jpg"
+
+     },
+     {
+      id: "15", nome: "Emoji o filme ", notaRotten: "6%", imagem : "emoji.jpg "
+
+
+     },
+
+];
