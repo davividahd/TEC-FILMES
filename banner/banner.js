3e4b7a6a65a3baedfec1066221194fbd67cbf78a
davividahd <daviricardosantos2007@gmail.com>
2025-04-02 15:33:02 -0300

    Add files via upload

---
+++ banner/banner.js
@@ -0,0 +1,13 @@
+import { Text, View, Image } from 'react-native';
+import styles from './style';
+export default function Banner() {
+    return (
+<View>
+    <Text style={styles.textBanner}> Em cartaz   </Text>
+    
+
+
+</View>
+
+    );
+}
---
+++ banner/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  
+      textBanner: {
+        fontSize: 45,
+        color: "white",
+        marginTop: 15,
+        marginLeft: -8,
+        fontWeight: "bold"
+    
+}
+ });
+export default styles;
