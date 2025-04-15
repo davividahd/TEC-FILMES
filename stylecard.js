8efa250268f5cbb46ac8fa1c644fbf43ed65302f
davividahd <daviricardosantos2007@gmail.com>
2025-04-15 16:49:10 -0300

    Create stylecard.js

---
+++ stylecard.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  
+    imagecard: {
+        width: '30%',
+        height: '40%',
+        marginTop: 210,
+        borderRadius: 10,
+    
+    }
+
+    
+});
+export default styles;
