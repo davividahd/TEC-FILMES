8b71e5d5b2c8a7416b301257388fe33e0efc0512
davividahd <daviricardosantos2007@gmail.com>
2025-05-27 16:23:16 -0300

    Update index.js

--- paginas/detalhes/index.js
+++ paginas/detalhes/index.js
@@ -1,9 +1,35 @@
-import { StyleSheet, Text, View, TextInput, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 export default function Detalhes() {
+  const Rota = useRoute();
   return (
     <View style={styles.container}>
-      <Text>Detalhes</Text>
+      <Image source={{ uri: Rota.params.imagem }} style={{ width: 250, height: 400 }} />
+      <Text  styles={styles.titulo} >{Rota.params.titulo}</Text>
+      <Text style = {styles.nota} > Nota: {Rota.params.nota}</Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141a29',
+    alignItems: 'center',
+  
+  },
+
+
+  titulo: {
+    fontSize: 40,
+   color: 'white',
+   
+   
+  },
+  nota: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
