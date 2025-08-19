82cccb254486aa7f177442938a8272c36939da9d
davividahd <daviricardosantos2007@gmail.com>
2025-08-19 16:16:48 -0300

    Update card.js

--- card/card.js
+++ card/card.js
@@ -1,13 +1,18 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View , TouchableOpacity } from 'react-native';
 import styles from './stylecard.js';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Card({ titulo, nota, imagem }) {
+  const navigation = useNavigation();
   return (
-    <View >
+    
+    <TouchableOpacity onPress={() => navigation.navigate('Detalhes', { title: titulo, vote_average: nota, poster_path: `https://image.tmdb.org/t/p/original${imagem}` })} style={styles.container}>
       
       <Image style={styles.imagecard} source={{ uri:(`https://image.tmdb.org/t/p/original${imagem}`) }} />
       <Text style= {styles.textcard}>{titulo}</Text>
       <Text style={styles.textnota}  > Nota: {nota}</Text>
-    </View>
+    </TouchableOpacity>
+
+    
   );
 }
